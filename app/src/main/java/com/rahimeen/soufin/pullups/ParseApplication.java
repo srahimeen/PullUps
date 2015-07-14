a9570837b847fb1d1b16c28e3e1ecca9670d2313
Soufin Rahimeen <soufinrahimeen@gmail.com>
2015-07-14 03:53:57 -0400

    Validation for end button.

--- app/src/main/java/com/rahimeen/soufin/pullups/ParseApplication.java
+++ app/src/main/java/com/rahimeen/soufin/pullups/ParseApplication.java
@@ -17,7 +17,7 @@ public class ParseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        //ParseCrashReporting.enable(this);
+        //ParseCrashReporting.enable(this); ok
         //ParseObject.registerSubclass(ParseWorkout.class);
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "MeMuuS9hfDM3LvnqettvWmt9alcr0qLwZIuDqvpw", "bCEERDTH8VrFpI0hD8pR5SXSUQmrlpB9oK8DWYMQ");
